339b421304e6569a89b42a71484daf5b10ff5cb9
zhuwj <byzhubaiyuan@gmail.com>
2025-08-02 21:28:44 +0800

    Generate the Database Migration

--- alembic/versions/275d6fb74bbd_initial_migration.py
+++ alembic/versions/275d6fb74bbd_initial_migration.py
@@ -1,18 +1,15 @@
 """Initial migration
 
 Revision ID: 275d6fb74bbd
-Revises: 
+Revises:
 Create Date: 2025-08-01 22:10:14.804984
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '275d6fb74bbd'
+revision: str = "275d6fb74bbd"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
