86880ed46d87da8537fc669e726a862dba329454
zhuwj <byzhubaiyuan@gmail.com>
2025-08-03 20:26:06 +0800

    Add foundational network

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
